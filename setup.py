27f891bc5953bece75a26823f9eeaca9cd7fc9a4
Sonu Kumar <sonukumarraj007@gmail.com>
2025-07-09 00:16:16 +0530

    initial-commit

--- setup.py
+++ setup.py
@@ -5,10 +5,18 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[],
-    author="Your Name",
-    author_email="your@email.com",
+    author="Sonu Kumar",
+    author_email="skrdeveloper1@gmail.com",
     description="Holiday and Trading Day Calendar for NIFTY",
-    url="https://github.com/yourusername/niftyTradeCalendar",
-    classifiers=["Programming Language :: Python :: 3"],
+    url="https://github.com/skrdeveloper1/niftyTradeCalendar",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    project_urls={
+        "LinkedIn": "https://in.linkedin.com/in/skrdeveloper1",
+        "GitHub": "https://github.com/skrdeveloper1/niftyTradeCalendar",
+    },
     python_requires=">=3.6",
 )
+https://in.linkedin.com/in/skrdeveloper1
